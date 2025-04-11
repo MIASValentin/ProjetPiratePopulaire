5c78a161cc8bfe6ee9990a782dfe707bde88e4c6
Enzo Salazar <enzo.j.salazar@gmail.com>
2025-04-11 17:07:55 +0200

    Create main.java
    Squelette de la boucle de jouabilité

---
+++ NF_PPP/src/test/main.java
@@ -0,0 +1,35 @@
+package test;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Initialisiation
+		//création des decks
+		
+		//boucle qui alterne entre les deux pirates :
+		
+		//	Choisir bundle
+		
+		//	Piocher 4 cartes
+		
+		//	Boucle de gameplay : tant que carte joués <2 || tour pas passé
+		
+		//		option de jouer une carte
+		//		si carte joué -> piocher 1 carte
+		
+		//		option de passer le tour
+		
+		//	Vérifier condition de victoire
+		//	vie de l'adversaire ?
+		//	prime à 1 millions ?
+		
+		// shuffle la main dans le deck
+		
+		//fin de partie : gloire au gagnant et honte au vaincu
+		
+
+	}
+
+}
