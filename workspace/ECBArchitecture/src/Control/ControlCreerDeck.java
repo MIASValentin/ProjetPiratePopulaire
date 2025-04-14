b3f2aa9b83bf0615e258f85fbc44da68db698d46
popoulpe <54855619+popoulpe@users.noreply.github.com>
2025-04-14 15:21:19 +0200

    Debut Control  (14/04)

---
+++ workspace/ECBArchitecture/src/Control/ControlCreerDeck.java
@@ -0,0 +1,10 @@
+package Control;
+
+import InterfaceNF.IFabriqueDeCarte;
+
+public class ControlCreerDeck {
+	IFabriqueDeCarte generateur;
+	
+	public void creerDeck();
+	public void ajouterBundle();
+}
---
+++ workspace/ECBArchitecture/src/Control/ControlCreerPirate.java
@@ -0,0 +1,13 @@
+package Control;
+
+import InterfaceNF.IPirate;
+
+public class ControlCreerPirate {
+	IPirate joueur1;
+	IPirate joueur2;
+	
+	public void creerPirate() {
+		//joueur1 = new IPirate();
+	}
+
+}
