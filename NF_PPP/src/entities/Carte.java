80a863b8844fe592ac5db8fc6d40579f18a1e4d4
Enzo Salazar <enzo.j.salazar@gmail.com>
2025-04-14 10:40:24 +0200

    classe partie + update abstract carte

--- NF_PPP/src/entities/Carte.java
+++ NF_PPP/src/entities/Carte.java
@@ -1,24 +1,28 @@
 package entities;
 
+import java.util.function.BiConsumer;
+
 public abstract class Carte implements ApplyCard {
 	private TypeCarte type;
 	private String nom; 
 	private String description; 
 	
-	private int EffetJoueurPv; 
-	private int EffetJoueurPrime;
+	//private int EffetJoueurPv; 
+	//private int EffetJoueurPrime;
 	
-	private int EffetAutreJoueurPv; 
-	private int EffetAutreJoueurPrime; 
+	//private int EffetAutreJoueurPv; 
+	//private int EffetAutreJoueurPrime; 
 	
-	BiFunction<Integer, Integer, Integer> applyEffetPv = (PvJoueur, effet) -> pvJoueur + effet;  
+	//BiFunction<Integer, Integer, Integer> applyEffetPv = (PvJoueur, effet) -> pvJoueur + effet;  
 	
-	ApplyCard apply = (Pirate j1, Pirate j2) -> {
+	/*ApplyCard apply = (Pirate j1, Pirate j2) -> {
 		j1.setPv(applyEffetPv());
 		j1.setPrime(EffetJoueurPrime); 
 		j2.setPv(EffetAutreJoueurPv);
 		j2.setPrime(EffetAutreJoueurPrime);
-	}; 
+	};*/
+	
+	public abstract BiConsumer<Pirate, Pirate> apply();
 	
 	
 }
