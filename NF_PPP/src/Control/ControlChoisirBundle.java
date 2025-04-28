068be4a21542e4981d58fefcf2f58e2d73c1ce3f
petarvukosavljevic <petar.vukosavljevic@univ-tlse3.fr>
2025-04-28 21:11:43 +0200

    ControlChoisirBundle

--- NF_PPP/src/Control/ControlChoisirBundle.java
+++ NF_PPP/src/Control/ControlChoisirBundle.java
@@ -1,10 +1,27 @@
 package Control;
 
+import entities.Pioche;
+import entities.Pirate;
+import entities.Carte;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ControlChoisirBundle {
 
-	public void selectionnerBundle() {
-		// TODO Auto-generated method stub
-		
-	}
+    private Pioche pioche;
 
+    public ControlChoisirBundle(Pioche pioche) {
+        this.pioche = pioche;
+    }
+
+    public void selectionnerBundle(Pirate pirate, int nbCartes) {
+        List<Carte> bundle = new ArrayList<>();
+        for (int i = 0; i < nbCartes; i++) {
+            Carte carte = pioche.donnerCarte();
+            if (carte != null) {
+                bundle.add(carte);
+            }
+        }
+        pirate.getDeck().addAll(bundle);
+    }
 }
