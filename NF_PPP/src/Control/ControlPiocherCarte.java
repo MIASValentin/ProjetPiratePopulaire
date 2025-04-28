77210f81f0420ac802ab6a31c514c08942c72902
Mohamed <83366998+FreezePunk@users.noreply.github.com>
2025-04-28 11:37:39 +0200

    Mise à jour ControlPiocherCarte

--- NF_PPP/src/Control/ControlPiocherCarte.java
+++ NF_PPP/src/Control/ControlPiocherCarte.java
@@ -1,14 +1,32 @@
 package Control;
 
-import InterfaceNF.IPirate;
+import entities.Pioche;
+import entities.Pirate;
+import entities.Carte;
 
 public class ControlPiocherCarte {
-	
-	public ControlPiocherCarte() {
-		
-	}
 
-	public void piocherCarte(IPirate joueurCourant, int i) {
-		
-	}
+    private Pioche pioche;
+    private Pirate[] pirates; // Liste des pirates (joueurs)
+
+    public ControlPiocherCarte(Pioche pioche, Pirate[] pirates) {
+        this.pioche = pioche;
+        this.pirates = pirates;
+    }
+
+    public void piocherCarte(int i) {
+        if (i < 0 || i >= pirates.length) {
+            System.out.println("Erreur : pirate inexistant.");
+            return;
+        }
+
+        Carte cartePiochee = pioche.piocher();
+        if (cartePiochee == null) {
+            System.out.println("La pioche est vide !");
+            return;
+        }
+
+        pirates[i].ajouterCarteMain(cartePiochee);
+        System.out.println("Le pirate " + i + " a pioché : " + cartePiochee.getNom());
+    }
 }
--- workspace/ECBArchitecture/src/Control/ControlPiocherCarte.java
+++ workspace/ECBArchitecture/src/Control/ControlPiocherCarte.java
@@ -1,32 +1,14 @@
 package Control;
 
-import entities.Pioche;
-import entities.Pirate;
-import entities.Carte;
+import InterfaceNF.IPirate;
 
 public class ControlPiocherCarte {
+	
+	public ControlPiocherCarte() {
+		
+	}
 
-    private Pioche pioche;
-    private Pirate[] pirates; // Liste des pirates (joueurs)
-
-    public ControlPiocherCarte(Pioche pioche, Pirate[] pirates) {
-        this.pioche = pioche;
-        this.pirates = pirates;
-    }
-
-    public void piocherCarte(int i) {
-        if (i < 0 || i >= pirates.length) {
-            System.out.println("Erreur : pirate inexistant.");
-            return;
-        }
-
-        Carte cartePiochee = pioche.piocher();
-        if (cartePiochee == null) {
-            System.out.println("La pioche est vide !");
-            return;
-        }
-
-        pirates[i].ajouterCarteMain(cartePiochee);
-        System.out.println("Le pirate " + i + " a pioché : " + cartePiochee.getNom());
-    }
+	public void piocherCarte(IPirate joueurCourant, int i) {
+		
+	}
 }
