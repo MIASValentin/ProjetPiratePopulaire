8850b5ad02d27a1cafa0826b1c3a11e68091be55
Mohamed <83366998+FreezePunk@users.noreply.github.com>
2025-04-29 17:18:18 +0200

    ControlJouerCarte

--- NF_PPP/src/Control/ControlJouerCarte.java
+++ NF_PPP/src/Control/ControlJouerCarte.java
@@ -4,15 +4,19 @@ import InterfaceNF.IPirate;
 
 public class ControlJouerCarte {
 
-	public void jouerBateau(IPirate joueurCourant) {
-		// TODO Auto-generated method stub
-		
-	}
+    /**
+     * Joue une carte classique pour le pirate courant.
+     */
+    public void jouerCarte(IPirate joueurCourant) {
+        joueurCourant.jouerCarte();
+    }
 
-	public void jouerCarte(IPirate joueurCourant) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
+    /**
+     * Joue une carte "bateau" pour le pirate courant.
+     */
+    public void jouerBateau(IPirate joueurCourant) {
+        // On réutilise la même logique que pour une carte classique,
+        // l'implémentation de jouerCarte gérant le type bateau.
+        joueurCourant.jouerCarte();
+    }
 }
