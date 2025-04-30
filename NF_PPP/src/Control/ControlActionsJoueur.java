9186897734bc02c291fb2ab91ee6b2ea0dabb5e8
Alex Tsan <tsl4297a@ensfsi.onmicrosoft.com>
2025-04-30 09:39:35 +0200

    ControlActionsJoueur - ControlCarte

--- NF_PPP/src/Control/ControlActionsJoueur.java
+++ NF_PPP/src/Control/ControlActionsJoueur.java
@@ -4,33 +4,25 @@ import entities.Carte;
 import entities.Pioche;
 import entities.Pirate;
 
-//Classe Test
+import java.util.List;
+
 public class ControlActionsJoueur {
-	private Pirate joueur; 
-	private int numeroJoueur; 
-	private Pioche pioche; 
-	
-	
-	public void changerPv(int value) {
-		joueur.setPv(joueur.getPv() + value);
-	}
-	
-	public void changerPime(int value) {
-		joueur.setPrime(joueur.getPrime() + value);
-	}
-	
-	public void piocherCarte(Pioche pioche, int nbCarteAPiocher) {
-		 for (int i = 0; i < nbCarteAPiocher; i++) {
-		        Carte piochee = pioche.donnerCarte();
-		        if (piochee != null) {
-		            this.main.add(pioche);
-		        }
-		    }
-	}
-	
-	
-	public void piocherCarte(Pioche pioche, int nbCarteAPiocher) {
-	   
-	}
-	
+    
+    public void changerPv(Pirate joueur, int value) {
+        joueur.setPv(joueur.getPv() + value);
+    }
+    
+    public void changerPrime(Pirate joueur, int value) {
+        joueur.setPrime(joueur.getPrime() + value);
+    }
+
+    public void piocherCarte(Pirate joueur, Pioche pioche, int nbCarteAPiocher) {
+        List<Carte> main = joueur.getMain();
+        for (int i = 0; i < nbCarteAPiocher; i++) {
+            Carte piochee = pioche.donnerCarte();
+            if (piochee != null) {
+                main.add(piochee);
+            }
+        }
+    }
 }
