9f31fd6fe2471d9cbbec3184eb613ca98fe3a4e4
Mohamed <83366998+FreezePunk@users.noreply.github.com>
2025-05-15 08:58:55 +0200

    Entities Carte Review

--- src/entities/Carte.java
+++ src/entities/Carte.java
@@ -5,20 +5,6 @@ public abstract class Carte {
     protected String nom; 
     protected String description; 
 
-//<<<<<<< HEAD
-//    //private int EffetJoueurPv; 
-//    //private int EffetJoueurPrime;
-//
-//    //private int EffetAutreJoueurPv; 
-//    //private int EffetAutreJoueurPrime; 
-//
-//    
-//
-//    
-//  
-//
-//=======
-//>>>>>>> 77b3eab9d2003edede2e220afb00983af0e94135
     public TypeCarte getType() {
         return type;
     }
